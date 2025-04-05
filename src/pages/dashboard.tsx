be4297f116c2eef8b15b557c75793b48bedb37a1
Md Nayeem <codernayeem898@gmail.com>
2025-04-05 19:58:54 +0600

    cleared boilerplates & prepared all

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,13 @@
+function Dashboard() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold">
+          Hi, I'm <span className="text-yellow-400">Md. Nayeem</span>!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
